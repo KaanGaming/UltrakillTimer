ef5984a82a9d7c80f95a58c9db4be200335ab668
kaan <kaangaming.gamejolt@gmail.com>
2024-02-28 20:00:44 +0300

    Add project files.

---
+++ UltrakillTimer/TimerController.cs
@@ -0,0 +1,46 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace UltrakillTimer
+{
+	public class TimerController : MonoBehaviour
+	{
+		private float _timer;
+		public Run.FixedTimeStamp timerEnd;
+		public Color originalColor;
+		public Color flashColor;
+		public float fadeTime;
+		public float delay;
+		//public AudioSource sync;
+		private TextMeshProUGUI timerText;
+		private TextMeshProUGUI headerText;
+
+		public void Awake()
+		{
+			timerText = transform.GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
+			headerText = transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+		}
+
+		public void Update()
+		{
+			_timer += Time.deltaTime;
+			float remainingTime = timerEnd.timeUntilClamped;
+			string[] timerstr = remainingTime.ToString(CultureInfo.InvariantCulture).Split('.');
+			timerText.text = $"{timerstr[0]}<size=40%>.{timerstr[1].Substring(0, timerstr[1].Length > 2 ? 2 : timerstr[1].Length)}";
+			timerText.color = Color.Lerp(flashColor, originalColor, _timer % (fadeTime + delay));
+			headerText.color = Color.Lerp(flashColor, originalColor, _timer % (fadeTime + delay));
+		}
+
+		public void ResetTimer()
+		{
+			_timer = 0f;
+		}
+	}
+}
---
+++ UltrakillTimer/UltrakillTimerPlugin.cs
@@ -0,0 +1,87 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace UltrakillTimer
+{
+	[BepInPlugin("com.kanggamming.ultrakilltimer", "ULTRAKILL Timer", "0.1.0")]
+    public class UltrakillTimerPlugin
+		: BaseUnityPlugin
+    {
+		public UltrakillTimerPlugin()
+			: base()
+		{
+
+		}
+
+		private AssetBundle _ab;
+		private GameObject _timer;
+
+		public void Awake()
+		{
+			Logger.LogInfo("Loading asset bundle");
+			_ab = AssetBundle.LoadFromMemory(GetEmbeddedResource("ultrakilltimerbundle"));
+			Logger.LogInfo("Loading timer prefab...");
+			_timer = _ab.LoadAsset<GameObject>("assets/critical failure timer.prefab");
+			Logger.LogInfo($"Loaded prefab {_timer.name} successfully.");
+		}
+
+		public void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.P))
+			{
+				GameObject canvasobj = new GameObject("Timer Canvas");
+				var canvas = canvasobj.AddComponent<Canvas>();
+				canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+				canvas.referencePixelsPerUnit = 100;
+				canvas.scaleFactor = 1;
+
+				var timer = GameObject.Instantiate(_timer, canvas.transform);
+				var ctrl = timer.AddComponent<TimerController>();
+				ctrl.delay = 0;
+				ctrl.fadeTime = 1;
+				ctrl.flashColor = new Color(1, 1, 0, 1);
+				ctrl.originalColor = new Color(1, 0, 0, 1);
+				ctrl.timerEnd = 80;
+			}
+		}
+
+		private byte[] GetEmbeddedResource(string name)
+		{
+			Logger.LogInfo($"Getting resource {name}");
+			Assembly asm = Assembly.GetExecutingAssembly();
+			byte[] output = null;
+			string resourceName = null;
+
+			foreach (string str in asm.GetManifestResourceNames())
+			{
+				if (str.Contains(name))
+					resourceName = str;
+			}
+			
+			if (string.IsNullOrEmpty(resourceName))
+			{
+				Logger.LogError($"Resource {resourceName} not found");
+				throw new ArgumentException("Embedded resource not found", nameof(name));
+			}
+
+			Logger.LogInfo($"Found resource {resourceName}");
+
+			using (Stream s = asm.GetManifestResourceStream(resourceName))
+			{
+				output = new byte[s.Length];
+				s.Read(output, 0, output.Length);
+				s.Dispose();
+			}
+
+			return output;
+		}
+    }
+}
